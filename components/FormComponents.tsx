8a6eb2bc729fa1e602963854d07080b9d060d3dd
vincent <hkvincentchan@gmail.com>
2024-04-12 22:43:02 +0800

    fix date format

--- components/FormComponents.tsx
+++ components/FormComponents.tsx
@@ -107,7 +107,7 @@ export function CustomFormDatePicker({ name, control, type, labelText }: CustomF
             {type === 'date' ? (
               <DatePicker
                 selected={field.value ? new Date(field.value) : null}
-                onChange={(date: any) => field.onChange(date)}
+                onChange={date => field.onChange(date)}
                 className="appearance-none block px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 dateFormat="MMMM d, yyyy"
               />
